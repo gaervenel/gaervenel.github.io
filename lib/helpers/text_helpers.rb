d328e34f07d347637f1826537cc26d18ae2e2b36
Gaervenel DM <gaervenel@gmail.com>
2020-02-05 12:07:01 -0800

    Add some text helpers

---
+++ lib/helpers/text_helpers.rb
@@ -0,0 +1,23 @@
+module TextHelpers
+
+  IGNORE_TITLE_CASE = %w(and for from in of on or the to)
+
+  # Capitalize each word, excluding IGNORE_TITLE_CASE words unless it's the
+  # first word
+  def titleize str
+    str.split(' ')
+      .each_with_index
+      .map { |w, i| (i > 0 && IGNORE_TITLE_CASE.include?(w)) ? w : w.capitalize }
+      .join(' ')
+  end
+
+  def ordinal num
+    case num
+    when 1 then "#{num}<sup>st</sup>"
+    when 2 then "#{num}<sup>nd</sup>"
+    when 3 then "#{num}<sup>rd</sup>"
+    else "#{num}<sup>th</sup>"
+    end
+  end
+
+end
